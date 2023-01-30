f96f267eb3a35fac5fa824bbe8468e52c5606b55
monoamine11231 <monoamine11231@gmail.com>
2023-01-30 11:23:01 +0100

    Ray-object intersection methods implemented for planes and spheres

---
+++ src/objects/plane.cc
@@ -0,0 +1,25 @@
+#include <sycl/sycl.hpp>
+#include "include/objects/plane.h"
+
+
+std::optional<Intersector> Plane::Intersect(const Ray& ray) const {
+    std::optional<Intersector> intersection;
+    float determinant, t;
+
+    determinant = sycl::dot(this->normal_, ray.dir);
+    if (determinant == 0.0f) {
+        /* If the ray and plane are paralell, return empty intersection */
+        return intersection;
+    }
+
+    t = sycl::dot(this->point_-ray.origin, this->normal_)/determinant;
+    if (t <= 0.0f) {
+        /*  If the intersection point is on the opposite of ray direction
+            return an empty intersection. */
+        return intersection;
+    }
+
+    Intersector data(t, this->normal_, this->material_id_);
+    intersection = data;
+    return intersection;
+}
