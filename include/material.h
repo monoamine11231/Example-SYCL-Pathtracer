2abfe629b6517b235a9a1e65bfd760491514a423
monoamine11231 <monoamine11231@gmail.com>
2024-02-23 16:01:00 +0100

    Switched to only using lambert diffuse

--- include/material.h
+++ include/material.h
@@ -7,7 +7,7 @@
 #include "include/utils.h"
 
 /* For debugging */
-const bool kUseAngleForSampling = false;
+const bool kUseAngleForSampling = true;
 
 namespace material {
 /* Microfacet material model */
@@ -29,7 +29,7 @@ struct MicrofacetMaterial {
 
   float fresnel0;
 
-  MicrofacetMaterial(sycl::vec<float, 3> base_color, float metallic,
+  SYCL_EXTERNAL MicrofacetMaterial(sycl::vec<float, 3> base_color, float metallic,
                      float roughness, bool dielectric, float reflectance,
                      float emmitance)
       : fresnel(Fresnel()),
@@ -46,67 +46,68 @@ struct MicrofacetMaterial {
      normal and the view direction. `h` and `l` are overwritten by the sampled
      halfway vector and the new direction vector */
   template <class Random>
-  void Sample(Random &random, const sycl::vec<float, 3> &v,
+  SYCL_EXTERNAL void Sample(Random &random, const sycl::vec<float, 3> &v,
               const sycl::vec<float, 3> &n, sycl::vec<float, 3> &h,
               sycl::vec<float, 3> &l) {
     this->normal.SampleHVec(*this, random(), random(), n, h);
     /* Sampled outgoing direction (light direction) */
-    l = 2 * sycl::dot(v, h) * h - v;
+    l = h;
   }
 
-  /* Returns the BRDF/PDF from the given new direction vector `l`, view vector
-   * `v`, geometric normal `n` and halfway vector `h`*/
-  template <class Random>
-  float Eval(const sycl::vec<float, 3> &l, const sycl::vec<float, 3> &v,
+  /* Returns the summation element for the monte carlo estimator */
+  SYCL_EXTERNAL float Eval(const sycl::vec<float, 3> &l, const sycl::vec<float, 3> &v,
              const sycl::vec<float, 3> &n, const sycl::vec<float, 3> &h) {
     /* Fresnel term in BRDF */
     sycl::vec<float, 3> F = this->fresnel(*this, h, l);
     /* Geometric term in BRDF */
-    float G = this->geometry(*this, l, v, h);
+    float G = this->geometry(*this, l, v, n, h);
 
-    float nv_dot = sycl::clamp(sycl::dot(n, v), 0.0f, 1.0f);
-    float hn_dot = sycl::clamp(sycl::dot(h, n), 0.0f, 1.0f);
-    /* Denominator term */
-    float denominator = sycl::clamp(sycl::dot(v, n), 0.0f, 1.0f) *
-                        sycl::clamp(sycl::dot(h, n), 0.0f, 1.0f);
+    if (sycl::dot(n,l) > 0.0f && sycl::dot(l, h) > 0.0f) {
+      /* Denominator term */
+      float denominator = sycl::dot(n, v)*
+                          sycl::dot(n, h);
 
-    return F.s0() * G * sycl::clamp(sycl::dot(v, h), 0.0f, 1.0f) / denominator;
+      return G * sycl::fabs(sycl::dot(v, h)) / denominator;
+    }
+    return 0.0f;
   }
 };
 
 /* Function objects */
 class FresnelSchlick {
-  FresnelSchlick(){};
+public:
+  SYCL_EXTERNAL FresnelSchlick(){};
 
   /* One fresnel for both dielectric and metallic materials */
   /* `n` is the halfway vector when applying the Microfacet model */
   template <class Fresnel, class Normal, class Geometry>
-  sycl::vec<float, 3> operator()(
+  SYCL_EXTERNAL sycl::vec<float, 3> operator()(
       const MicrofacetMaterial<Fresnel, Normal, Geometry> &material,
       const sycl::vec<float, 3> &n, const sycl::vec<float, 3> &v) {
-    sycl::vec<float, 3> F0{material.fresnel0, material.fresnel0,
-                           material.fresnel0};
+    sycl::vec<float, 3> F0{0.0f, 0.0f,
+                           0.0f};
 
     /* Linear mix f0 and base color based on metaliness */
     F0 = vecutils::Lerp(F0, material.base_color, material.metallic);
 
     sycl::vec<float, 3> ones{1.0f, 1.0f, 1.0f};
-    float rdot = 1 - sycl::dot(n, v);
+    float rdot = 1.0f - sycl::clamp(sycl::dot(n, v), 0.0f, 1.0f);
 
     return F0 + (ones - F0) * (rdot * rdot * rdot * rdot * rdot);
   }
 };
 
 class NormalGGX {
-  NormalGGX(){};
+public:
+  SYCL_EXTERNAL NormalGGX(){};
   /* `n` is the halfway vector when applying the Microfacet model */
   template <class Fresnel, class Geometry>
-  float operator()(
+  SYCL_EXTERNAL float operator()(
       const MicrofacetMaterial<Fresnel, material::NormalGGX, Geometry> &material,
       const sycl::vec<float, 3> &n, const sycl::vec<float, 3> &h) {
     float alpha = material.roughness * material.roughness;
     float alpha_sq = alpha * alpha;
-    float dot = sycl::dot(n, h);
+    float dot = sycl::clamp(sycl::dot(n, h), 0.0f, 1.0f);
 
     float tmp = dot * dot * alpha_sq + (1 - dot * dot);
     return (miscutils::ShadowFactor(dot) * alpha_sq) / (M_PI * tmp * tmp);
@@ -114,28 +115,30 @@ class NormalGGX {
 
   /* Returns whole angles for theta and phi */
   template <class Fresnel, class Geometry>
-  std::pair<float, float> Sample(
+  SYCL_EXTERNAL std::pair<float, float> Sample(
       const MicrofacetMaterial<Fresnel, material::NormalGGX, Geometry> &material,
       float u1, float u2) {
     float alpha = material.roughness * material.roughness;
 
     /* Sampling of normal distribution function */
-    float theta = atan((alpha * sqrt(u1)) / sqrt(1 - u1));
-    float phi = 2 * M_PI * u2;
 
-    return std::pair<float, float>(theta, phi);
+    float phi = u1 * M_PI_2;
+    float theta = sycl::atan(alpha*sycl::sqrt(u1 / (1.0f - u1)));
+    theta = u2 * 2 * M_PI;
+
+    return std::pair<float, float>(phi, theta);
   }
 
   /* Returns theta in cos and sin, while phi is returned as an angle. It is more
    * efficient to compute this */
   template <class Fresnel, class Geometry>
-  std::tuple<float, float, float> SampleTrigonometric(
+  SYCL_EXTERNAL std::tuple<float, float, float> SampleTrigonometric(
       const MicrofacetMaterial<Fresnel, NormalGGX, Geometry> &material,
       float u1, float u2) {
     float alpha = material.roughness * material.roughness;
 
-    float cos_theta = sqrt((1 - u1) / (1 + (alpha * alpha - 1) * u1));
-    float sin_theta = sqrt(1 - cos_theta * cos_theta);
+    float cos_theta = sycl::sqrt((1 - u1) / (1 + (alpha * alpha - 1) * u1));
+    float sin_theta = sycl::sqrt(1 - cos_theta * cos_theta);
     float phi = 2 * M_PI * u2;
 
     return std::make_tuple(cos_theta, sin_theta, phi);
@@ -144,7 +147,7 @@ class NormalGGX {
   /* Sample the microfacet normal by given geometric normal, `h` is the output
    */
   template <class Fresnel, class Geometry>
-  void SampleHVec(
+  SYCL_EXTERNAL void SampleHVec(
       const MicrofacetMaterial<Fresnel, NormalGGX, Geometry> &material,
       float u1, float u2, const sycl::vec<float, 3> &n,
       sycl::vec<float, 3> &h) {
@@ -159,16 +162,16 @@ class NormalGGX {
 
     /* Needed for estimating in Monte Carlo integration */
     if (kUseAngleForSampling) {
-      auto[theta, phi] = this->Sample(material, u1, u2);
-      _x = plane_x * sin(theta) * cos(phi);
-      _y = plane_y * sin(theta) * sin(phi);
-      _z = n * cos(theta);
+      auto[phi, theta] = this->Sample(material, u1, u2);
+      _x = plane_x * sycl::sin(phi) * sycl::cos(theta);
+      _y = plane_y * sycl::sin(phi) * sycl::sin(theta);
+      _z = n * sycl::cos(phi);
 
     } else {
       auto[cos_theta, sin_theta, phi] =
           this->SampleTrigonometric(material, u1, u2);
-      _x = plane_x * sin_theta * cos(phi);
-      _y = plane_y * sin_theta * sin(phi);
+      _x = plane_x * sin_theta * sycl::cos(phi);
+      _y = plane_y * sin_theta * sycl::sin(phi);
       _z = n * cos_theta;
     }
     /* Halfway vector between ingoing and outgoing directions */
@@ -177,10 +180,11 @@ class NormalGGX {
 };
 
 class GeometryGGXSchlick {
-  GeometryGGXSchlick(){};
+public:
+  SYCL_EXTERNAL GeometryGGXSchlick(){};
   /* `n` is the halfway vector when applying the Microfacet model */
   template <class Fresnel, class Geometry>
-  float operator()(
+  SYCL_EXTERNAL float operator()(
       const MicrofacetMaterial<Fresnel, NormalGGX, Geometry> &material,
       const sycl::vec<float, 3> &l, const sycl::vec<float, 3> &v,
       const sycl::vec<float, 3> &n, const sycl::vec<float, 3> &h) {
@@ -188,7 +192,7 @@ class GeometryGGXSchlick {
   }
 
   template <class Fresnel, class Geometry>
-  float partial(
+  SYCL_EXTERNAL float partial(
       const MicrofacetMaterial<Fresnel, NormalGGX, Geometry> &material,
       const sycl::vec<float, 3> &x, const sycl::vec<float, 3> &n,
       const sycl::vec<float, 3> &h) {
